f869adbcc7eb549c525ff5fa636f5d4c402af8ed
Alice Bevan–McGregor <alice@gothcandy.com>
2016-03-06 19:24:16 -0500

    Marrow-standard package metadata structure.

--- setup.py
+++ setup.py
@@ -1,46 +1,104 @@
 #!/usr/bin/env python3
+# encoding: utf-8
 
 import os
 import sys
+import codecs
 
-from transcrypt.modules.org.transcrypt import __base__
+try:
+	from setuptools.core import setup, find_packages
+except ImportError:
+	from setuptools import setup, find_packages
 
-from setuptools import setup
+from setuptools.command.test import test as TestCommand
 
-def read(*paths):
-	with open (os.path.join (*paths), 'r') as aFile:
-		return aFile.read()
 
-setup (
-	name = 'transplit',
-	version = __base__.__envir__.transpilerVersion,
-	description = "A Pythonic Python to Javascript transpiler derived from Transcrypt.",
-	long_description = (
-		read('README.rst') + '\n\n' +
-		read('transcrypt', 'license_reference.txt')
-	),
-	keywords = ['python', 'javascript', 'transpiler', 'compiler'],
-	url = "https://github.com/marrow/transplit,
+if sys.version_info < (3, 5):
+	raise SystemExit("Python 3.5 or later is required.")
+
+exec(open(os.path.join("transcrypt", "release.py")).read())
+
+
+class PyTest(TestCommand):
+	def finalize_options(self):
+		TestCommand.finalize_options(self)
+		
+		self.test_args = []
+		self.test_suite = True
+	
+	def run_tests(self):
+		import pytest
+		sys.exit(pytest.main(self.test_args))
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+py2 = sys.version_info < (3,)
+py26 = sys.version_info < (2, 7)
+py32 = sys.version_info > (3,) and sys.version_info < (3, 3)
+
+tests_require = ['coverage', 'pytest', 'pytest-cov', 'pytest-spec', 'pytest-flakes']
+
+
+# # Entry Point
+
+setup(
+	name = "transplit",
+	version = version,
+	
+	description = description,
+	long_description = codecs.open(os.path.join(here, 'README.rst'), 'r', 'utf8').read(),
+	url = url,
+	
+	author = author.name,
+	author_email = author.email,
+	
 	license = 'Apache 2.0',
-	author = 'Alice Bevan-McGregor and Contributors',
-	author_email = 'alice@gothcandy.com',
-	packages = ['transcrypt'],
-	include_package_data = True,
-	zip_safe = False,
-	entry_points = {
-		'console_scripts': ['transcrypt = transcrypt.__main__:__main__']
-	},
+	keywords = '',
 	classifiers = [
-		'Development Status :: 3 - Alpha',
-		'Intended Audience :: Developers',
-		'Natural Language :: English',
-		'License :: Apache Software License',
-		'Topic :: Software Development :: Compilers',
-		'Topic :: Software Development :: Build Tools',
-		'Topic :: Software Development :: Pre-processors',
-		'Topic :: Software Development :: Libraries :: Python Modules',
-		'Operating System :: OS Independent',
-		'Programming Language :: Python :: 3.5',
-		'Programming Language :: JavaScript'
-	],
+			"Development Status :: 3 - Alpha",
+			"Intended Audience :: Developers",
+			"License :: Apache Software License",
+			"Topic :: Software Development :: Compilers",
+			"Topic :: Software Development :: Build Tools",
+			"Topic :: Software Development :: Pre-processors",
+			"Topic :: Software Development :: Libraries :: Python Modules",
+			"Operating System :: OS Independent",
+			"Programming Language :: Python :: 3.5",
+			"Programming Language :: JavaScript",
+		],
+	
+	packages = find_packages(exclude=['test', 'doc', 'example', 'benchmark']),
+	include_package_data = True,
+	package_data = {'': ['README.rst', 'LICENSE.txt']},
+	namespace_packages = [],
+	
+	# ## Dependency Declaration
+	
+	install_requires = [
+		],
+	
+	extras_require = dict(
+			development = tests_require,
+		),
+	
+	tests_require = tests_require,
+	
+	# ## Plugin Registration
+	
+	entry_points = {
+			'console_scripts': [  # Command-line interface.
+						'transplit = transplit.__main__:__main__'
+					],
+			'transplit.override': [  # Having these here allows for multiple packages to cooperate easily.
+						'itertools = transplit.stdlib.itertools',
+					],
+			},
+	
+	zip_safe = False,
+	cmdclass = dict(
+			test = PyTest,
+		)
 )
+
+# L-CLEAN
